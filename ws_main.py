ba93422328984e618894265ecbd4cae87fc2f262
stephens-isonishi <chrisk.isonishi@gmail.com>
2019-08-22 16:51:19 +0900

    added functions that empty folder when restarting experiment, use last modified file

--- ws_main.py
+++ ws_main.py
@@ -14,32 +14,49 @@ import datetime
 import json
 import os
 import re
+import os
+import shutil
+import glob
 
 
 FILEPATH = '/kw_resources/food/model_weights/'
 BATCH_SIZE = 256
 NUM_EPOCHS = 5
 
+def empty_folder():
+    folder = FILEPATH
+    for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
+
 def find_most_recent_model():
-    recent = 0
-    ans = ""
+    if not os.listdir(FILEPATH):
+        return "", 0
+    list_of_files = glob.glob(FILEPATH + '*') # * means all 
+    latest_file = max(list_of_files, key=os.path.getctime) #could be min depending on os
 
-    for file in os.listdir(FILEPATH):
-
-        temp = int(re.search(r'\d+', file).group(0))
-        if recent < temp:
-            recent = temp
-            ans = file
-    return ans, recent
+    count = len([1 for x in list(os.scandir(FILEPATH)) if x.is_file()])
+    return latest_file, count
 
 def main():
+    reset_training = False
+    if reset_training:
+        empty_folder()
+
     np.random.seed(45)
     nb_class = 451 #found by doing: echo */ | wc
     width, height = 224, 224
     bat_size = BATCH_SIZE
 
+    #structure
     sn = model.SqueezeNet(nb_classes=nb_class, inputs=(3, height, width))
 
+    #multi-gpu
     local_devices = device_lib.list_local_devices()
     num_gpus = len([dev.name for dev in local_devices if dev.device_type == 'GPU'])
     print(num_gpus)
@@ -47,13 +64,15 @@ def main():
         sn = multi_gpu_model(sn, num_gpus)
     print('build model')
 
+
+
     #obviously mess around with the hyperparameters
     #sgd = SGD(lr = .001, decay=.0002, momentum=.9, nesterov=True)
-    current_epoch_num = 0
+
     num_files = 0
     saved_model, current_epoch_num = find_most_recent_model()
-    print("saved model: " + saved_model + " current epoch: {}".format(current_epoch_num))
-    if len(saved_model) > 0 and current_epoch_num > 1:
+    print("saved model: " + saved_model + "current epoch: {}".format(current_epoch_num))
+    if len(saved_model) > 0 and current_epoch_num != 0:
         sn = load_model(FILEPATH + saved_model)
 
 
@@ -65,7 +84,7 @@ def main():
     training_dir = '/kw_resources/food/dataset/training_data/'
     validation_dir = '/kw_resources/food/dataset/testing_data/'
     
-    num_training = 166580  #use find . -type f | wc -l for each directory
+    num_training = 166580  #used find . -type f | wc -l for each directory
     num_validation = 60990
     
     num_epochs = NUM_EPOCHS
@@ -106,6 +125,7 @@ def main():
         mode='max')
     callbacks_list = [checkpoint]
 
+    #fitting
     sn.fit_generator(
         train_data,
         steps_per_epoch=(num_training // BATCH_SIZE),
@@ -115,11 +135,12 @@ def main():
         callbacks=callbacks_list,
         verbose=1)
 
+    #save history to use at a later time
     history = sn
     with open('/kw_resources/food/results/e:{}_b:{}_{}'.format(num_epochs, bat_size, datetime.datetime.now().strftime('%m-%d-%X')), 'wb') as f:
         pickle.dump(history.history, f)
 
-   # sn.save_weights('/kw_resources/food/results/weights.h5')
+
 
 if __name__ == '__main__':
     main()
