d33412ea6523516ded0c4d7a7a34e4c4de599990
stephens-isonishi <chrisk.isonishi@gmail.com>
2019-08-23 14:10:20 +0900

    indentation error

--- ws_main.py
+++ ws_main.py
@@ -26,13 +26,13 @@ NUM_EPOCHS = 20
 def empty_folder():
     folder = FILEPATH
     for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
-    try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
-        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-    except Exception as e:
-        print(e)
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
 
 def find_most_recent_model():
     if not os.listdir(FILEPATH):
