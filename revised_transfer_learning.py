6d8c921a894529249ecc5304ff4dd62650b20d0a
stephens-isonishi <chrisk.isonishi@gmail.com>
2019-10-24 16:04:00 +0900

    indentation error

--- revised_transfer_learning.py
+++ revised_transfer_learning.py
@@ -75,7 +75,8 @@ def clean_directory(directory):
 		try:
 			if os.path.isfile(file_path):
 				os.unlink(file_path)
-            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
 		except Exception as e:
 			print(e)
 
