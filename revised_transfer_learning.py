5d38174205b08c671baddb539c543985a9996c8e
stephens-isonishi <chrisk.isonishi@gmail.com>
2019-10-24 16:27:19 +0900

    indentation issue

--- revised_transfer_learning.py
+++ revised_transfer_learning.py
@@ -72,13 +72,13 @@ def clean_directory(directory):
 	source = directory
 	for files in os.listdir(source):
 		file_path = os.path.join(source, files)
-		try:
-			if os.path.isfile(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-		except Exception as e:
-			print(e)
+		#try:
+		if os.path.isfile(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+		#except Exception as e:
+        #print(e)
 
 
 #transfer learning to adapt it to dataset classes
