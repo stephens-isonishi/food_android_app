cc8d82f0d8c0d12ca6342e1e94c49c1cc9ad547d
stephens-isonishi <chrisk.isonishi@gmail.com>
2019-11-20 16:36:00 +0900

    finished everything. only problem is true label number is not available due to shuffling the data with imagedatagenerator during training

--- validatingAndVisualization.py
+++ validatingAndVisualization.py
@@ -6,17 +6,16 @@ validation and visualization of model results so it wouldn't be a "black box"
 import os, sys
 #import matplotlib
 # matplotlib.use('agg')
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import argparse
 import numpy as np
 from datetime import datetime
 import platform
-import glob
-import shutil
-import tensorflow as tf
+import cv2
+# import tensorflow as tf
 
 # from IPython.display import display
-# from PIL import Image
+from PIL import Image
 from keras import backend as K
 from keras import regularizers
 from keras import __version__
@@ -38,38 +37,49 @@ def center_crop(image, center_crop_size):
     return image[centerw - halfw: centerw + halfw + 1, centerh - halfh: centerh + halfh + 1, :]
 
 
-def predict_crop(img, img_num, show_images=True, debugging=True, model):
+def predict_crop(model, img, actual_label, show_images=True, debugging=True):
 
+	res = cv2.resize(img, dsize=(299,299), interpolation=cv2.INTER_CUBIC)
 
 	crops = [
 	    img[:299,:299, :], # Upper Left
         img[:299, img.shape[1]-299:, :], # Upper Right
         img[img.shape[0]-299:, :299, :], # Lower Left
         img[img.shape[0]-299:, img.shape[1]-299:, :], # Lower Right
-        center_crop(img, (299, 299))
+        center_crop(img, (299, 299)), #center region
+        res #downscaled image to 299 x 299 size
 	]
 	if show_images:
-		figure, axes = plt.subplots(3,2, figsize=(8,4))
-		figure.delaxes(axes[1,2])
+		figure, axes = plt.subplots(2,3, figsize=(8,4))
+		#figure.delaxes(axes[1,2])
 		axes[0][0].imshow(crops[0])
 		axes[0][1].imshow(crops[1])
 		axes[0][2].imshow(crops[2])
 		axes[1][0].imshow(crops[3])
 		axes[1][1].imshow(crops[4])
+		axes[1][2].imshow(crops[5])
 
-	y_preds = model.predict(np.array(crops))
-    print(y_preds)	
+
+	y_preds = model.predict(np.array(crops)) 
+	top_pred = np.argmax(y_preds, axis=1)
+	top_5_preds = np.argpartition(y_preds, -5)[:,-5:]
 
 	if debugging:
-		print('top 1 predictions:', preds)
+		print('top 1 predictions:', top_pred)
 		print('top 5 predictions:', top_5_preds)
-		print('true label:', y_test[img_num])
+		print('true label:', actual_label)
 
 
 
 def main():
     model = load_model(WEIGHT_FILE)
+    image_name = '../testing_data/pizza/2003290.jpg'
+    image = np.array(Image.open(image_name))
 
+    ### actual label is not available currently, due to shuffling data with ImageDataGenerator in revised_transfer_learning.py
+    ### may need to retrain to get this data and generate the confusion matrix and whatnot
+    
+    predict_crop(model,image, 'xyz')
 
 if __name__ == '__main__':
 	main()
